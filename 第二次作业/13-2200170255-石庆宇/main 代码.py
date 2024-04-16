4fb9a2f61231189bd934296d1e97856d9a24ab93
shiqingyu8 <165243305+shiqingyu8@users.noreply.github.com>
2024-04-16 16:29:48 +0800

    Update main 代码.py

--- 第二次作业/13-2200170255-石庆宇/main 代码.py
+++ 第二次作业/13-2200170255-石庆宇/main 代码.py
@@ -1,8 +1,5 @@
-# 这是一个示例 Python 脚本。
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
- 重要性 愚蠢的 作为 核蛋白
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib
 from matplotlib import rcParams  ## run command settings for plotting
@@ -16,12 +13,12 @@ config = {
 rcParams.update(config)  ## 设置画图的一些参数
 ## 读取数据
 path = 'E:/软件/QQ/regress_data1.csv'
-import pandas as pd
 data = pd.read_csv(path) ## data 是dataframe 的数据类型
 data.head() # 返回data中的前几行数据，默认是前5行。
 cols = data.shape[1]
 X_data = data.iloc[:,:cols-1]#X是所有行，去掉最后一列， 未标准化
 y_data = data.iloc[:,cols-1:]#X是所有行，最后一列
+
 data.plot(kind='scatter', x='人口', y='收益', figsize=(4,3)) # 利用散点图可视化数据
 import matplotlib
 plt.xlabel('人口')
@@ -31,44 +28,44 @@ X_data.insert(0, 'Ones', 1)
 X_data.head()
 y_data.head()
 X=X_data.values
- Y=y_data. 价值观
- W=np. 排列       (       [       [       0.0       ] , [       0.0       ]       ]       )        ## 初始化W系数矩阵，w 是一个(2,1)矩阵
- 德福        计算机主机       ( X、Y、W、R ) :
-    Y_hat = np. 点       ( X,W )
-    W1 = W [ :- 1       ]
-    loss =np. 概括的       (       ( Y_Hat-Y ) ** 2       ) / (      2 *X. 形状     [    0   ]  )  # (m,n) @ (n, 1) -> (n, 1)
-    L2_正规化=       ( 审查员/ 2       ) * np. 概括的       ( W1** 2       )
-    损失=损失+L2_规则化
-     返回的 损失
- 德福        梯度的       ( X,Y,W,阿尔法,雷格 ) :
-    电话:火车=X。 形状 [      0     ]
-    Y_hat = np. 点    ( X, W )
-    数字=X。 T @ ( Y_Hat-Y ) /     # 计算原始梯度
+Y=y_data.values
+W=np.array([[0.0],[0.0]]) ## 初始化W系数矩阵，w 是一个(2,1)矩阵
+def computeCost(X, Y, W,reg):
+    Y_hat = np.dot(X,W)
+    W1 = W[:-1]
+    loss =np.sum((Y_hat - Y)** 2)/(2*X.shape[0]) # (m,n) @ (n, 1) -> (n, 1)
+    l2_regularization = (reg/2)* np.sum(W1** 2)
+    loss=loss+l2_regularization
+    return loss
+def gradientDescent(X, Y, W, alpha, reg):
+    num_train = X.shape[0]
+    Y_hat = np.dot(X, W)
+    dW = X.T @ (Y_hat - Y) / num_train  # 计算原始梯度
     # 提取权重矩阵W中除了偏置项以外的部分
-    W_reg = W [ :- 1  ]
+    W_reg = W[:-1]
     # 计算L2正则化项的梯度（不包括偏置项）
-    DW_Reg=REG*W_Reg
+    dW_reg = reg * W_reg
     # 将正则化项的梯度加到除了偏置项以外的原始梯度中
-    dW_with_reg = np.        连接        (        ( 德国, [ 二重水 [ - 1        ]        ]        ) , axis= 0        )          # 保持偏置项的梯度不变
-    W -= alpha * dW_with_reg   
-     返回的 W
- 德福         线性回归        ( 先生,先生,先生,先生 ) :
-    卢什_希= [        ]
-    #步骤1:初始化模型参数
-    特征_dim=x。 形状 [        1       ]
-    W=np. 零度      (     ( 特点: 1    )   )   ## 初始化W系数矩阵，w 是一个(feature_dim,1)矩阵
-    #重复步骤2和步骤3,直到迭代的收敛或结束
-     为了 我 在…中        范围        ( 伊特斯 ) :
-        #步骤2:使用被排除的参数预测产量并计算损失
-        损失= 计算机主机      ( X,Y,W,reg )
-        他的。 附加     ( 损失 )
-        #步骤3:使用梯度象限方法更新参数
-        W= 梯度的    ( X、Y、W、阿尔法、Reg )
-     返回的 loss_his, W ## 返回损失和模型参数。
-来自 线上模型 重要性 线性回归
+    dW_with_reg = np.concatenate((dW_reg, [dW[-1]]), axis=0)  # 保持偏置项的梯度不变
+    W -= alpha * dW_with_reg  # 使用梯度下降更新参数
+    return W
+def linearRegression(X,Y, alpha, iters,reg):
+    loss_his = []
+    # step1: initialize the model parameters
+    feature_dim = X.shape[1]
+    W=np.zeros((feature_dim,1)) ## 初始化W系数矩阵，w 是一个(feature_dim,1)矩阵
+    ## repeat step 2 and step 3 untill to the convergence or the end of iterations
+    for i in range (iters):
+        # step2 : using the initilized parameters to predict the output and calculate the loss
+        loss = computeCost(X,Y,W,reg)
+        loss_his.append(loss)
+        # step3: using the gradient decent method to update the parameters
+        W=gradientDescent(X, Y, W, alpha,reg)
+    return loss_his, W ## 返回损失和模型参数。
+from sklearn.linear_model import LinearRegression
 
 
- 德福    预言  ( X, W ) :
+def predict(X, W):
     '''
     输入：
         X：测试数据集
@@ -94,6 +91,13 @@ ax.set_ylabel('收益', rotation=90)
 ax.set_title('引入L2后的梯度下降预测收益和人口规模')
 plt.show()
 
+fig, ax = plt.subplots(figsize=(6, 4))
+ax.plot(np.arange(iters), loss_his, 'r')
+ax.set_xlabel('迭代次数')
+ax.set_ylabel('代价', rotation=0)
+ax.set_title('引入L2后的误差和训练Epoch数')
+plt.show()
+
 def zxec(X,Y):
     model=LinearRegression()
     model.fit(X,Y)
@@ -113,12 +117,7 @@ def zxec(X,Y):
     plt.show()
 zxec(X,Y)
 
-fig, ax = plt.subplots(figsize=(6, 4))
-ax.plot(np.arange(iters), loss_his, 'r')
-ax.set_xlabel('迭代次数')
-ax.set_ylabel('代价', rotation=0)
-ax.set_title('误差和训练Epoch数')
-plt.show()
+
 
 #归一化
 from sklearn.preprocessing import MinMaxScaler
