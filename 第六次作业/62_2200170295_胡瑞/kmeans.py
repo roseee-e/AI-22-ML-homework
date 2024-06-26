36e90bee3697f1cdadf85a55e69a639cfc5ede71
Yu Guoli <3144880940@qq.com>
2024-06-26 19:03:00 +0800

    Update kmeans.py

--- 第六次作业/62_2200170295_胡瑞/kmeans.py
+++ 第六次作业/62_2200170295_胡瑞/kmeans.py
@@ -1,20 +1,27 @@
 import numpy as np
 import cv2
-from skimage.color import rgb2gray
-from skimage.filters import rank
+from sklearn.cluster import KMeans
 
 # 读取图像
 image = cv2.imread('hana.jpg')
 
 
-# 将图像数据转换为灰度图
-gray_image = rgb2gray(image)
+# 将图像数据转换为二维数组
+pixel_values = image.reshape((-1, 3))
 
-# 使用滤波器增加图像模糊度
-filtered = rank.median(gray_image, np.ones((3, 3)))
+# 使用K均值算法聚类
+kmeans = KMeans(n_clusters=5, random_state=42)
+kmeans.fit(pixel_values)
 
-# 显示原始图像和模糊图像
+# 获取每个像素点所属的聚类中心
+segmented_img = kmeans.cluster_centers_[kmeans.labels_]
+segmented_img = np.clip(segmented_img.astype('uint8'), 0, 255)  # 确保在0-255范围内
+
+# 将分割后的图像重新转换为原始尺寸
+segmented_img = segmented_img.reshape(image.shape)
+
+# 显示原始图像和分割后的图像
 cv2.imshow('Original Image', image)
-cv2.imshow('Segmented Image (FCM)', filtered)
+cv2.imshow('Segmented Image (K-means)', segmented_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
